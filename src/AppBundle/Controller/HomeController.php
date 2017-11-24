c6ee2d2bbdaba03e0e1f62e065bd8c5587168cfd
delny <delgado.anthony@gmail.com>
2017-11-24 11:00:24 +0100

    create template and route for homepage

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anthony
+ * Date: 24/11/2017
+ * Time: 10:42
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/", name="home")
+     */
+    public function indexAction (Request $request)
+    {
+        return $this->render(':home:index.html.twig',[]);
+    }
+}
